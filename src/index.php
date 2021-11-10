6089482c72093b97b694685e4f097fc66235f8a1
Groctel <git@taxorubio.com>
2021-11-10 10:41:24 +0100

    Feat: Top navigation bar common to all pages

--- src/index.php
+++ src/index.php
@@ -3,9 +3,14 @@
 ini_set('display_errors', 1);
 require_once('vendor/autoload.php');
 
-$loader = new \Twig\Loader\FilesystemLoader('templates');
-$twig   = new \Twig\Environment($loader);
+function main ()
+{
+	$loader = new \Twig\Loader\FilesystemLoader('templates');
+	$twig   = new \Twig\Environment($loader);
 
-echo $twig->render('index.html');
+	echo $twig->render('index.html');
+}
+
+main();
 
 ?>
