de2ef298f2104794f9894531dcdbf6eaf4ef109d
Groctel <git@taxorubio.com>
2021-12-21 13:20:05 +0100

    Feat: Generate QR when shortening a link. Closes #2.

--- src/url-result.php
+++ src/url-result.php
@@ -4,6 +4,22 @@ ini_set("display_errors", 1);
 require_once("vendor/autoload.php");
 require_once("php/url.php");
 
+use chillerlan\QRCode\{QRCode, QROptions};
+
+function generate_qr (URL $url)
+{
+	$qr_path    = "cache/qr/qr-" . $url->handle() . ".png";
+	$qr_options = new QROptions([
+		'outputType'       => QRCode::OUTPUT_IMAGE_PNG,
+		'eccLevel'         => QRCode::ECC_L,
+		'imageTransparent' => false,
+		'pngCompression'   => 9,
+	]);
+
+	(new QRCode($qr_options))->render($url->full_handle(), $qr_path);
+	return $qr_path;
+}
+
 function read_form ()
 {
 	return array(
@@ -18,11 +34,13 @@ function main ()
 	$twig   = new \Twig\Environment($loader);
 
 	$form_fields = read_form();
-	$url = new URL($form_fields["url"], $form_fields["handle"]);
+	$url         = new URL($form_fields["url"], $form_fields["handle"]);
+	$qr_path     = generate_qr($url);
 
 	echo $twig->render("url-result.html", [
 		"index_tab" => "active-tab",
-		"url" => $url,
+		"url"       => $url,
+		"qr_path"   => $qr_path,
 	]);
 }
 
