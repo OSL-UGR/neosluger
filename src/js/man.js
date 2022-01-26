dd708dee650bff15ef63cab058470d0d44fb35b9
Groctel <git@taxorubio.com>
2022-01-26 11:46:54 +0100

    Docs: Updated API specification

--- src/js/man.js
+++ src/js/man.js
@@ -1,35 +1,3 @@
-function switch_api ()
-{
-	const new_api = document.querySelector("#new-api");
-	const old_api = document.querySelector("#old-api");
-
-	const new_api_style = window.getComputedStyle(new_api);
-	const old_api_style = window.getComputedStyle(old_api);
-
-	const switch_api = document.querySelector("#switch-api");
-	const inner_html = switch_api.innerHTML;
-
-	if (new_api_style.display === "none")
-	{
-		new_api.style.display = "contents";
-		switch_api.innerHTML = inner_html.replace("nueva", "antigua");
-	}
-	else
-	{
-		new_api.style.display = "none";
-	}
-
-	if (old_api_style.display === "none")
-	{
-		old_api.style.display = "contents";
-		switch_api.innerHTML = inner_html.replace("antigua", "nueva");
-	}
-	else
-	{
-		old_api.style.display = "none";
-	}
-}
-
 function switch_tab (event, tabname)
 {
 	// Toggles the visible manual content
