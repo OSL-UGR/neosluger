169614ae085cd25979e577d1e16631e3c7a0d50b
Groctel <git@taxorubio.com>
2022-02-02 13:42:35 +0100

    Refactor: Big interface changes
    This commit is huge because it is the result of two days of finding UX
    errors and fixing them all at once for coherence. If I haven't missed
    anything, this should be the final version of the website.

--- src/pages/inspect-result.php
+++ src/pages/inspect-result.php
@@ -7,13 +7,13 @@ require_once($_SERVER['DOCUMENT_ROOT']."/php/const.php");
 
 function filter_short_link_handle (string $query): string
 {
-	$handle = $query;
-	$query_no_https  = preg_replace("/https?:\/\//", "", $query);
+	$handle = trim($query);
+	$query_no_https  = trim(preg_replace("/https?:\/\//", "", $query));
 
 	if (str_starts_with($query_no_https, Neosluger\SITE_ADDRESS))
 		$handle = substr($query_no_https, strlen(Neosluger\SITE_ADDRESS), strlen($query));
 
-	return $handle;
+	return trim($handle);
 }
 
 
